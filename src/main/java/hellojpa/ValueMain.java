7673f4c374cd306f5b21e787b8fffd7ac27d879c
joona95 <joona95@naver.com>
2023-09-19 22:59:22 +0900

    feat: 기본값 타입

---
+++ src/main/java/hellojpa/ValueMain.java
@@ -0,0 +1,19 @@
+package hellojpa;
+
+public class ValueMain {
+
+    public static void main(String[] args) {
+
+        Integer a = 10;
+        Integer b = a;
+
+        /*
+        int a = 10;
+        int b = a;
+        a = 20;
+        */
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
